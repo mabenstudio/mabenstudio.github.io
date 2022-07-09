814f40211402617741a8f5e218bf516f835d1bb1
Majzik Bence <redfellipe@gmail.com>
2022-07-09 16:24:44 +0200

    new

--- js/main.js
+++ js/main.js
@@ -61,3 +61,29 @@ $(document).ready(function () {
         $("header").toggleClass("menu-btn-open");
     });
 });
+
+var textWrapper = document.querySelector(".top-text-anim");
+textWrapper.innerHTML = textWrapper.textContent.replace(
+    /\S/g,
+    "<span class='letter'>$&</span>"
+);
+
+anime
+    .timeline({ loop: true })
+    .add({
+        targets: ".top-text-anim .letter",
+        translateX: [40, 0],
+        translateZ: 0,
+        opacity: [0, 1],
+        easing: "easeOutExpo",
+        duration: 1200,
+        delay: (el, i) => 500 + 30 * i,
+    })
+    .add({
+        targets: ".top-text-anim .letter",
+        translateX: [0, -30],
+        opacity: [1, 0],
+        easing: "easeInExpo",
+        duration: 1100,
+        delay: (el, i) => 100 + 30 * i,
+    });
